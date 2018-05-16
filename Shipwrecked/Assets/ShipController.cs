2673ed1d8fc81fabe2a17a535f3ecda7d976e629
itwatts <itwatts@calpoly.edu>
2018-05-16 13:26:43 -0700

    Added Base Sprites

---
+++ Shipwrecked/Assets/ShipController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShipController : MonoBehaviour {
+	private Rigidbody rb;
+	private float orientation = 0;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void FixedUpdate () {
+		float moveHorizontal = Input.GetAxis("Horizontal");
+		float moveVertical = Input.GetAxis("Vertical");
+
+		Vector2 movement = new Vector2(moveHorizontal, moveVertical);
+		rb.AddForce(movement);
+	}
+}
